2f56de7b6c65d26ec56d24a97a2280368f6b20ee
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2025-06-28 16:27:16 +0200

    Added form validations for first step of mutli form

--- src/app/components/multi-step-form/multi-step-form.component.ts
+++ src/app/components/multi-step-form/multi-step-form.component.ts
@@ -7,15 +7,75 @@ import {
   Utensils,
 } from 'lucide-angular';
 import { DropDownComponent } from '../shared/drop-down/drop-down.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-multi-step-form',
-  imports: [CurrentStepInfoComponent, LucideAngularModule, DropDownComponent],
+  imports: [
+    CurrentStepInfoComponent,
+    LucideAngularModule,
+    DropDownComponent,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './multi-step-form.component.html',
   styleUrl: './multi-step-form.component.css',
 })
 export class MultiStepFormComponent {
+  form: FormGroup;
   step = signal(1);
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      step1: this.formBuilder.group({
+        name: ['', Validators.required],
+        cuisine: ['', Validators.required],
+        phoneNumber: [''],
+        website: [''],
+        description: ['', Validators.required],
+      }),
+    });
+  }
+
+  nextStep(event: Event) {
+    event.preventDefault();
+    if (this.step() <= 4 && this.isCurrentStepValid()) {
+      console.log(this.form.value);
+      this.step.update((prev) => prev + 1);
+    }
+  }
+
+  isCurrentStepValid() {
+    const stepGroup = this.getStepFormGroup();
+    stepGroup.markAllAsTouched();
+    return stepGroup.valid;
+  }
+
+  getStepFormGroup(): FormGroup {
+    return this.form.get(`step${this.step()}`) as FormGroup;
+  }
+
+  prevStep(event: Event) {
+    event.preventDefault();
+    if (this.step() > 1) {
+      this.step.update((prev) => prev - 1);
+    }
+  }
+
+  handleCuisineChange(cuisine: string) {
+    const step1 = this.form.get('step1') as FormGroup;
+    step1.get('cuisine')?.setValue(cuisine);
+  }
+
+  left = ChevronLeft;
+  right = ChevronRight;
+  utensils = Utensils;
 
   cuisines = [
     'American',
@@ -31,22 +91,4 @@ export class MultiStepFormComponent {
     'vegan',
     'other',
   ];
-
-  nextStep(event: Event) {
-    event.preventDefault();
-    if (this.step() <= 4) {
-      this.step.update((prev) => prev + 1);
-    }
-  }
-
-  prevStep(event: Event) {
-    event.preventDefault();
-    if (this.step() > 1) {
-      this.step.update((prev) => prev - 1);
-    }
-  }
-
-  left = ChevronLeft;
-  right = ChevronRight;
-  utensils = Utensils;
 }
